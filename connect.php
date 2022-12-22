51c8138a94cb9a4b048977bd46a0351766bebebd
Tae Yun <taeyun.koolung@gmail.com>
2022-12-22 10:12:09 -0400

    v8 connect.php touched up, uploaded to the server

--- connect.php
+++ connect.php
@@ -9,6 +9,7 @@
         $email = $_POST['email'];
         $from = "koolung22@gmail.com";
         $message = "The booking for ".$name." has been successfully made for ".$time." on ".$date." for ".$service." with the following details: ".$details."";
+        $messageAlert ="NAME: ".$name." TIME: ".$time." DATE: ".$date." SERVICE: ".$service." DETAILS: ".$details." EMAIL: ".$email."";
 
         $to = $email;
         $subject = "Beaute Lia Hair Booking Information";
@@ -31,8 +32,24 @@
                     </div>
                 </body>
                 </html>';
+        $messageAlert = '<!doctype html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport"
+                          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+                    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                    <title>Document</title>
+                </head>
+                <body>
+                <span class="preheader" style="color: transparent; display: none; height: 0; max-height: 0; max-width: 0; opacity: 0; overflow: hidden; mso-hide: all; visibility: hidden; width: 0;">'.$message.'</span>
+                    <div class="container">
+                     '.$messageAlert.'<br/>
+                    </div>
+                </body>
+                </html>';
         $result = @mail($to, $subject, $message, $headers);
-        $result = @mail($from, $subject, $message, $headers);
+        $result = @mail($from, $subject, $messageAlert, $headers);
 
         echo '<script>alert("Booking information sent to your email")</script>';
     }
